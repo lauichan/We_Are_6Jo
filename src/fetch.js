f5f55f7579663cb3c4b582aff7fda8ca7d1079dc
기다림 <gidalimforsomeday@gmail.com>
2024-01-10 21:10:29 +0900

    urls추가

--- src/fetch.js
+++ src/fetch.js
@@ -1,6 +1,8 @@
 import { createCard } from "./movie.js";
 import { apikey } from "./apikeys.js";
 
+let urls = "https://api.themoviedb.org/3/";
+
 export let genreList;
 
 export async function loadJSON(url) {
@@ -10,21 +12,19 @@ export async function loadJSON(url) {
 }
 
 export async function loadGenre() {
-  const response = await loadJSON(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apikey}&language=en`);
+  const response = await loadJSON(`${urls}genre/movie/list?api_key=${apikey}&language=en`); // 중복되는 url을 변수로 지정하면 추후에 변할 때도 적용이 가능하기때문에
   genreList = response.genres;
   console.log(genreList);
 }
 
 export async function loadPage(page) {
-  const data = await loadJSON(
-    `https://api.themoviedb.org/3/movie/top_rated?api_key=${apikey}&language=en&page=${page}`
-  );
+  const data = await loadJSON(`${urls}movie/top_rated?api_key=${apikey}&language=en&page=${page}`);
   createCard(data);
 }
 
 export async function searchPage(page, keyword) {
   const data = await loadJSON(
-    `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${keyword}&include_adult=false&language=en-US&page=${page}`
+    `${urls}search/movie?api_key=${apikey}&query=${keyword}&include_adult=false&language=en-US&page=${page}`
   );
   createCard(data);
 }
