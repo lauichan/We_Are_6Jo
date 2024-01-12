2da3ab5e8b060dae587616320495a6542dcf65ba
기다림 <gidalimforsomeday@gmail.com>
2024-01-12 16:08:19 +0900

    검수중

--- src/detail__.js
+++ src/detail__.js
@@ -8,7 +8,7 @@ async function load() {
   return moviePage(id);
 }
 
-console.log("생성체크용", load());
+load();
 
 // const paintCard = document.getElementById("detailCommentReviewWrap");
 const userId = document.getElementById("detailReviewUserId");
@@ -17,33 +17,27 @@ const userStar = document.getElementById("detailReviewStar");
 const submitBtn = document.getElementById("detailReviewSubmitBtn");
 const userText = document.getElementById("detailReviewContent");
 
-let reviewStorage = [];
-
-// console.log("ddddd");
-
 function sendReview(e) {
   e.preventDefault();
 
   // 리뷰키 유무 확인
-  // let ReviewList = {};
+  let ReviewList = {};
 
   if (window.localStorage.getItem(id) !== null) {
-    reviewStorage = JSON.parse(window.localStorage.getItem(id));
+    ReviewList = JSON.parse(window.localStorage.getItem(id));
   }
 
-  // // 리뷰키 사용자키 유무 확인
-  // let userKey = "ID_" + userId.value;
+  // 리뷰키 사용자키 유무 확인
+  let userKey = "ID_" + userId.value;
 
-  // if (!ReviewList.hasOwnProperty(userKey)) {
-  //   ReviewList[userKey] = {};
-  // }
+  if (!ReviewList.hasOwnProperty(userKey)) {
+    ReviewList[userKey] = {};
+  }
 
-  // ReviewList[userKey] = userReviewInfo();
+  ReviewList[userKey] = userReviewInfo();
 
-  userReviewInfo(id);
-  console.log(userReviewInfo(id));
   // 사용자 리뷰 내용 저장
-  window.localStorage.setItem(id, JSON.stringify(reviewStorage));
+  window.localStorage.setItem(id, JSON.stringify(ReviewList));
 
   loadReview();
 }
@@ -60,51 +54,41 @@ function userReviewInfo() {
 
 function loadReview() {
   const guestReview = document.getElementById("movieReview");
-
   guestReview.innerHTML = "";
-  //   guestReview = "";
 
-  const reviewStorage = JSON.parse(window.localStorage.getItem(id)) || [];
-  // console.log(ReviewList);
-  Object.values(reviewStorage).forEach((userReview) => {
+  const ReviewList = JSON.parse(window.localStorage.getItem(id)) || {};
+  Object.entries(ReviewList).forEach(([reviewId, review]) => {
     const entryHtml = `
-    <li id=>
+    <li id=${reviewId}>
       <div class="detail_comment_list_img">
         <div class="detail_comment_list_user">
-          <div class="detail_comment_list_user_id" id="userId">${userReview.userName}</div>
-          <div class="detail_comment_list_user_text" id="userInputComment">${userReview.content}</div>
-          <div class="detail_comment_list_user_star" id="userInputStar">${userReview.star}</div>
+          <div class="detail_comment_list_user_id" id="userId">${review.userName}</div>
+          <div class="detail_comment_list_user_text" id="userInputComment">${review.content}</div>
+          <div class="detail_comment_list_user_star" id="userInputStar">${review.star}</div>
         </div>
         <button id="deleteReview">삭제</button>
       </div>
     </li>`;
-
     guestReview.insertAdjacentHTML("beforeend", entryHtml);
   });
+  document.querySelectorAll("#deleteReview").forEach((button) => button.addEventListener("click", deleteReview));
 }
-function search(id) {
-  let keys = Object.keys(localStorage);
-  let setItem = [];
-
-  for (let i = 0; i < keys.length; i++) {
-    let key = keys[i];
-    if (key.includes(id)) {
-      let value = localStorage[key];
-      setItem.push(value);
-    }
-  }
-  return setItem;
-}
-
-let result = search(id);
-console.log(result);
 
 function deleteReview() {
-  window.localStorage.getItem(id.userKey);
-  console.log("키", window.localStorage.getItem(id.userKey));
+  let reviewId = this.closest("li").id;
+  console.log(reviewId);
   loadReview();
 }
+// value로 저장되는 값이  객체를 가지는데, 이 객체에 key값을 지정해서 잡아야함
+
+// 즉  value의 특정 key값을 제거해야함.
+
+//getItem으로 key에 해당하는 value를 가져와야함
+
+// key는 id로 통일되어있으니 window.localStorage.getItem(id, value) (이것은 작성한 userKey로 생성이 됨)
+
+const valuee = window.localStorage.getItem(id.userKey);
+console.log(valuee);
 
 // 페이지 로드시에 loadAndDisplay 함수 호출
 window.addEventListener("load", loadReview);
-// document.getElementById("deleteReview").addEventListener("click", deleteReview);
